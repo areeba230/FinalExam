53c77ba54f901a473dc55b02715d768594db49cb
areeba230 <areeba230>
2023-06-24 16:00:34 +0500

    mongodb_create_is_added

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const Recipe= require("./model");
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/hotelManagementSystem")
+.then(()=>{console.log("connected")})
+.catch((err)=>{console.log(err)});
 
 app.set("view engine","ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+
 
 app.get("/",(req,res)=>{
     res.render("home");
@@ -15,7 +24,10 @@ app.post("/",(req,res)=>{
     const {title,description,file} = req.body;
     const obj = {recipe_title:title, recipe_des:description,recipe_img:file}
    console.log(req.body)
-   res.render("submit",obj)
+   const rcp = Recipe.create(req.body);
+    if(!rcp){res.redirect("/")}
+    else{res.render("submit",{obj})}
+  
 })
 
 
